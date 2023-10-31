63bae2348aa411abfdb23c23c01e6525accd2657
aaditya-rathore <aadityarathore29@gmail.com>
2023-11-01 01:17:23 +0530

    completed creating subreddits

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -3,14 +3,17 @@
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { FC,useState } from "react";
 import { useMutation } from "@tanstack/react-query"
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { CreateSubredditPayload } from "@/lib/validators/subreddit";
+import { toast } from "@/hooks/use-toast";
+import { useCustomToast } from "@/hooks/use-custom-toast";
 
-const page =() =>{
+const page:FC =() =>{
     const [input,setInput] = useState<string>("")
     const router = useRouter()
+    const {loginToast} =useCustomToast()
 
     const {mutate:createCommunity, isLoading}= useMutation({
         mutationFn: async () => {
@@ -19,6 +22,38 @@ const page =() =>{
             }
            const {data}=await axios.post('/api/subreddit',payload)
            return data as string
+        },
+        onError:(err) =>{
+            if (err instanceof AxiosError){
+                if (err.response?.status === 409){
+                    return toast({
+                        title:'Subreddit already exists',
+                        description:'Please choose another name',
+                        variant:'destructive',
+                    })
+                }
+
+                if (err.response?.status === 422){
+                    return toast({
+                        title:'Invalid name.',
+                        description:'Please choose a name between 3 to 21 characters.',
+                        variant:'destructive',
+                    })
+                }
+
+                if(err.response?.status === 401){
+                    return loginToast()
+                }
+            }
+
+            toast({
+                title:'Something went wrong.',
+                description:'Could not create community.',
+                variant:'destructive',
+            })
+        },
+        onSuccess: (data) =>{
+            router.push(`/r/${data}`)
         },
     })
 
@@ -32,7 +67,7 @@ const page =() =>{
                 <hr className="bg-zinc-500 h-px"/> 
                 <div>
                     <p className="text-lg font-medium">Name</p>
-                    <p className="text-xm pd-2">
+                    <p className="text-xm pb-2">
                         Community names including capitalization cannot be changed.
                     </p>
 
@@ -48,14 +83,14 @@ const page =() =>{
                 </div>
 
                 <div className="flex justify-end gap-4">
-                    <Button variant='subtle' onclick={() => router.back()}>
+                    <Button disabled={isLoading} variant='subtle' onclick={() => router.back()}>
                         Cancel
                     </Button>
                     <Button
                      isLoading={isLoading}
                      disabled={input.length === 0}
-                     onClick={()=>createCommunity()}
-                     > Create Community</Button>
+                     onClick={()=>createCommunity()}>
+                     Create Community</Button>
                 </div> 
             </div>
         </div>
--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 const Providers = ({children}:{children:React.ReactNode}) => {
     const queryClient = new QueryClient()
+    
     return( 
         <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     )
